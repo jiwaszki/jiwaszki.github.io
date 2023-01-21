9a038658f335d2bfa447855cf540020f24e8fe1d
jiwaszki <jiwaszkiewicz6@gmail.com>
2023-01-21 02:11:18 +0100

    Configured Giscus comment section

--- src/lib/config/post.ts
+++ src/lib/config/post.ts
@@ -1,3 +1,19 @@
 import type { PostConfig } from '$lib/types/post'
 
-export const post: PostConfig = {}
+export const post: PostConfig = {
+    comment: {
+        use: ['Giscus'],
+        style: 'boxed', // comment system bar styles: none / bordered / lifted / boxed
+        giscus: {
+          repo: 'jiwaszki/jiwaszki.github.io',
+          repoID: 'R_kgDOIyERSg',
+          category: 'Comments',
+          categoryID: 'DIC_kwDOIyERSs4CTt5a',
+          reactionsEnabled: true, // reactions: true / false
+          inputPosition: 'top', // position of comment box: top / bottom
+          lang: 'en', // language
+          theme: 'preferred_color_scheme', // theme
+          loading: 'lazy',
+        }
+      }    
+}
