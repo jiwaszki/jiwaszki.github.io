5b81e19c74e30c74f95c4392bc59d2feb7e7048b
jiwaszki <jiwaszkiewicz6@gmail.com>
2023-01-21 02:11:49 +0100

    Added buttons to share and scroll to comments

---
+++ src/lib/components/actions/share.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { site } from '$lib/config/site'
+    export let post: Urara.Post
+
+    // How to get it to work?
+    // var a2a_config = a2a_config || {};
+    // a2a_config.onclick = 1;
+</script>
+
+<a
+    href={`https://www.addtoany.com/share#url=${site.protocol + site.domain + post.path}&title=${encodeURI(
+        post.title ?? post.path.slice(1)
+    )}`}
+    class="btn btn-lg btn-circle btn-ghost bg-base-100 shadow-lg hover:shadow-xl">
+   <span class="i-heroicons-outline-share" />
+</a>
